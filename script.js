b107819d4e02333c7bd7faff036d4ccb522988c0
leonardopea <leonardo.pea.1996@gmail.com>
2023-06-13 22:09:24 +0200

    tasks collegate all'arrey di js

--- script.js
+++ script.js
@@ -3,10 +3,44 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            titolo: 'Vue Todolist',
+
+            tasks:[
+                {
+                    testo: 'anguria',
+                    completato: false
+                },
+                {
+                    testo: 'aglio',
+                    completato: false
+                },
+                {
+                    testo: 'cipolla',
+                    completato: false
+                },
+                {
+                    testo: 'succo di frutta',
+                    completato: false
+                },
+                {
+                    testo: 'melograno',
+                    completato: false
+                },
+                {
+                    testo: 'noodles',
+                    completato: false
+                },
+                {
+                    testo: 'pomodoro',
+                    completato: false
+                },
+
+            ]
+
 
         }
     },
     methods: {
         
     },
-}).mount('app');
+}).mount('#app');
